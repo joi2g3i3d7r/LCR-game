23f344cc48854ce93220b9c13ff8c5b3bba08e79
Jorge Bizarro Reyes <jorgeluisbizarroreyes@gmail.com>
2023-03-11 19:28:26 -0500

    remove unnecesary comments

--- src/main.ts
+++ src/main.ts
@@ -18,29 +18,3 @@ rl.on('line', (line: string) => {
   game.start();
 
 });
-
-
-
-
-// const j1 = new Player();
-// const j2 = new Player();
-// const j3 = new Player();
-// const game = new Game([j1, j2, j3]);
-// const textInput = 'LR.CCR.L.RLLLCLR.LL..R...CLR.';
-// game.start(textInput.split(''));
-
-
-
-// const j1 = new Player();
-// const j2 = new Player();
-// const j3 = new Player();
-// const j4 = new Player();
-// const j5 = new Player();
-// const game = new Game([j1, j2, j3, j4, j5]);
-// const textInput = 'RL....C.L';
-// game.start(textInput.split(''));
-
-// const game = new Game([]);
-// const textInput = '';
-// game.start(textInput.split(''));
-
